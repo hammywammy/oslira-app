d703b7c12003bc412eac9604e660b855fae73165
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-13 18:16:01 -0700

    Create HowItWorksSection.tsx

---
+++ src/features/homepage/components/HowItWorksSection.tsx
@@ -0,0 +1,9 @@
+export function HowItWorksSection() {
+  return (
+    <section className="home__how-it-works">
+      <div className="home__section-container">
+        <h2>How It Works</h2>
+      </div>
+    </section>
+  );
+}
