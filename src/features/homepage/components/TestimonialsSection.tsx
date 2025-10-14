3ee8dde627ff8d4c505def63cf8473693d0bd2b0
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-13 18:15:05 -0700

    Create TestimonialsSection.tsx

---
+++ src/features/homepage/components/TestimonialsSection.tsx
@@ -0,0 +1,9 @@
+export function TestimonialsSection() {
+  return (
+    <section className="home__social-proof">
+      <div className="home__section-container">
+        <h2>Testimonials</h2>
+      </div>
+    </section>
+  );
+}
