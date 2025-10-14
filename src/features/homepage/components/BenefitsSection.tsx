b12c87ec86caf699b6e4b937701e47e8908760f8
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-13 18:16:19 -0700

    Create BenefitsSection.tsx

---
+++ src/features/homepage/components/BenefitsSection.tsx
@@ -0,0 +1,9 @@
+export function BenefitsSection() {
+  return (
+    <section className="home__benefits">
+      <div className="home__section-container">
+        <h2>Benefits Section</h2>
+      </div>
+    </section>
+  );
+}
