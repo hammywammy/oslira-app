0ffc8b6982f1af5fb768f0b822712447633ef052
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-13 22:10:43 -0700

    Update HomePage.tsx

--- src/pages/marketing/HomePage.tsx
+++ src/pages/marketing/HomePage.tsx
@@ -5,8 +5,6 @@
  * Path: src/pages/marketing/HomePage.tsx
  */
 
-import React from 'react';
-
 export function HomePage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-purple-950 to-slate-950">
