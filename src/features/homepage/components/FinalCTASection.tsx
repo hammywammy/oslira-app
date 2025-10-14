5cc8b92b7986f23fe808eb51e1ec35c0a47f2e95
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-13 18:15:29 -0700

    Create FinalCTASection.tsx

---
+++ src/features/homepage/components/FinalCTASection.tsx
@@ -0,0 +1,9 @@
+export function FinalCTASection() {
+  return (
+    <section className="home__final-cta">
+      <div className="home__section-container">
+        <h2>Get Started</h2>
+      </div>
+    </section>
+  );
+}
